c2001c5d748456be70894db0240e555a220251cd
Zecharias Mengis <zmengis@gmail.com>
2021-03-03 17:04:41 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,15 +1,11 @@
 def game_hash
   {
     :home =>
-      {:team_name => "Team Name"},
-      {:colors => ["Strings of colors"]},
+      {:team_name => "Brooklyn Nets"},
+      {:colors => ["Black", "White"]},
       {:players =>
         [
-         {:player_name => },
-         {:number => },
-         {:shoe => },
-         {:points => },
-         {:rebounds => },
+         {:player_name => "Alan Anderson"}, {:number => }, {:shoe => }, {:points => }, {:rebounds => },
          {:assists => },
          {:steals => },
          {:blocks => },
