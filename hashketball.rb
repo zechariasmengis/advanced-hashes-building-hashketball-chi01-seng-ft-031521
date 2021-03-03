6fb1177337f554535379536c7d8f643c6f7210b4
Zecharias Mengis <zmengis@gmail.com>
2021-03-03 17:05:44 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -5,9 +5,7 @@ def game_hash
       {:colors => ["Black", "White"]},
       {:players =>
         [
-         {:player_name => "Alan Anderson"}, {:number => }, {:shoe => }, {:points => }, {:rebounds => }, {:assists => }, {:steals => },
-         {:blocks => },
-         {:slam_dunks => }
+         {:player_name => "Alan Anderson"}, {:number => }, {:shoe => }, {:points => }, {:rebounds => }, {:assists => }, {:steals => }, {:blocks => }, {:slam_dunks => }
         ]
       },
     :away =>
