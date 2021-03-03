d5454a0f971965d938ecac98fcce4e147392640f
Zecharias Mengis <zmengis@gmail.com>
2021-03-03 17:29:34 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,9 @@
 def game_hash
   {
     :home =>
-      {:team_name => "Brooklyn Nets"},
-      {:colors => ["Black", "White"]},
-      {:players =>
+      {:team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players =>
         [
          {:player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
          {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
