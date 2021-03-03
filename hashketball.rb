5e0da29dac24ebc5be3fa7ba0716d8c2351d2704
Zecharias Mengis <zmengis@gmail.com>
2021-03-03 17:02:51 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -15,6 +15,22 @@ def game_hash
          {:blocks => },
          {:slam_dunks => }
         ]
+      },
+    :away =>
+      {:team_name => "Team Name"},
+      {:colors => ["Strings of colors"]},
+      {:players =>
+        [
+         {:player_name => },
+         {:number => },
+         {:shoe => },
+         {:points => },
+         {:rebounds => },
+         {:assists => },
+         {:steals => },
+         {:blocks => },
+         {:slam_dunks => }
+        ]
       }
   }
 end
