1c127e299b04821094e1e785b6f09732d724e563
Zecharias Mengis <zmengis@gmail.com>
2021-03-03 17:28:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -13,9 +13,9 @@ def game_hash
         ]
       },
     :away =>
-      {:team_name => "Charlotte Hornets"},
-      {:colors => ["Turquoise", "Purple"]},
-      {:players =>
+      {:team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players =>
         [
          {:player_name => "Jeff Adrien", :number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
          {:player_name => "Bismack Biyombo", :number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 22, :blocks => 15, :slam_dunks => 10},
